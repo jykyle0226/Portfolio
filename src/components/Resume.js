b20c76b99449fb35e6e1d5ed6d198bbe54199a57
jykyle0226 <jykyle0226@gmail.com>
2022-07-28 15:03:49 -0400

    resume

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -3,13 +3,13 @@ import React from "react";
 const Resume = () => {
   return (
     <div className="resume" >
-      <a>
+      <a href="https://drive.google.com/file/d/1uQNPtvr-pgzpJA_DsojDEW7eoK9vuK5N/view?usp=sharing">
         <div id="resume-div">
           <img id="resume" src="https://i.imgur.com/wgVGKU0.png" alt=""></img>
           <h2>Creative Resume</h2>
         </div>
       </a>
-      <a>
+      <a href="https://drive.google.com/file/d/1uQNPtvr-pgzpJA_DsojDEW7eoK9vuK5N/view?usp=sharing">
         <div>
           <img  className="resume-img" src="https://i.imgur.com/4nohCJd.png?1" alt=""></img>
           <h2>Scannable Resume</h2>
