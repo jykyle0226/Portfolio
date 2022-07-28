34b7ac06baca4787f16484a73dad01a3cccf6172
jykyle0226 <jykyle0226@gmail.com>
2022-07-28 17:12:41 -0400

    additional css

--- src/components/About.js
+++ src/components/About.js
@@ -3,13 +3,12 @@ import { Link } from "react-router-dom";
 import '../Style/About.css'
 const About = () => {
   return (
-    <div className="About-div">
-      <div className="name-div">
-        <h1 className="name"><span className="hello-span">Hello,</span></h1>
-        <h1 className="name">My name is Min Yong Kim</h1>
-      </div>
+    <div className="hi">
+      <div className="About-div">
       <div className="intro-div">
         <div >
+        <h1 className="name"><span className="hello-span">Hello,</span></h1>
+          <h1 className="name">My name is Min Yong Kim</h1>
           <h4 className="intro">
             I am a full-stack developer with a focus on JavaScript, React,
             Express, MongoDB, and Node.
@@ -21,14 +20,27 @@ const About = () => {
           </h4>
         </div>
         <div>
-          <h4 className="passion">
+          <h4 className="intro">
             With 4 years of experience in audio engineering, I possess a strong
             foundation in problem solving skills and result-oriented work ethic.
           </h4>
         </div>
       </div>
     </div>
+    <div className="contact">
+      <div className="phone-div">
+      <img src="https://i.imgur.com/2rkG6JN.png" alt="" className="phone"></img>
+      <h4 className="numb">(571)992-3010</h4>
+      </div>
+      <div className="email-div">
+      <img src="https://i.imgur.com/G2R0wIX.png" alt="" className="email" ></img>
+      <h4>min.y.kim@gmail.com</h4>
+      </div>
+      
+    </div>
 
+    </div>
+    
   );
 };
 
